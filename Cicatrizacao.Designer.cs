f777bdcf309382e28c84ddeb9db5f0a23373cc79
Leridia-Casa <leridia10@gmail.com>
2025-10-29 19:34:09 -0300

    Arrumei o tamanho da pagina cicatrização

--- Cicatrizacao.Designer.cs
+++ Cicatrizacao.Designer.cs
@@ -30,23 +30,23 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cicatrizacao));
             this.Pan1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.Pan2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Pan2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.Pan1.SuspendLayout();
-            this.Pan2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Pan2.SuspendLayout();
             this.SuspendLayout();
             // 
             // Pan1
@@ -62,10 +62,84 @@
             this.Pan1.Controls.Add(this.label3);
             this.Pan1.Controls.Add(this.label2);
             this.Pan1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Pan1.Location = new System.Drawing.Point(21, 154);
+            this.Pan1.Location = new System.Drawing.Point(21, 128);
             this.Pan1.Name = "Pan1";
-            this.Pan1.Size = new System.Drawing.Size(1071, 765);
+            this.Pan1.Size = new System.Drawing.Size(1010, 500);
             this.Pan1.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_163514131_removebg_preview;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(725, 49);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(180, 183);
+            this.pictureBox4.TabIndex = 18;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_163928313_removebg_preview;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(842, 191);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(168, 171);
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_163246609_removebg_preview;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(873, -45);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(161, 184);
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_164224623_removebg_preview;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(743, 305);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 202);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(711, 144);
+            this.label6.TabIndex = 4;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(674, 72);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "2. Fase Proliferativa: Começa por volta do 3º dia. Formação de novos vasos e\r\ntec" +
+    "ido de granulação (vermelho vivo, brilhante e úmido), indicando uma cicatriz\r\nim" +
+    "atura.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(673, 96);
+            this.label4.TabIndex = 2;
+            this.label4.Text = resources.GetString("label4.Text");
             // 
             // label3
             // 
@@ -81,7 +155,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(242, 12);
+            this.label2.Location = new System.Drawing.Point(251, 11);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(468, 24);
             this.label2.TabIndex = 0;
@@ -93,7 +167,7 @@
             this.Pan2.Controls.Add(this.label1);
             this.Pan2.Location = new System.Drawing.Point(21, 29);
             this.Pan2.Name = "Pan2";
-            this.Pan2.Size = new System.Drawing.Size(1071, 100);
+            this.Pan2.Size = new System.Drawing.Size(1010, 74);
             this.Pan2.TabIndex = 0;
             this.Pan2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -101,85 +175,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(267, 27);
+            this.label1.Location = new System.Drawing.Point(268, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(442, 49);
             this.label1.TabIndex = 0;
             this.label1.Text = "Processo de Cicatrização";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 96);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(673, 96);
-            this.label4.TabIndex = 2;
-            this.label4.Text = resources.GetString("label4.Text");
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 216);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(674, 72);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "2. Fase Proliferativa: Começa por volta do 3º dia. Formação de novos vasos e\r\ntec" +
-    "ido de granulação (vermelho vivo, brilhante e úmido), indicando uma cicatriz\r\nim" +
-    "atura.";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(36, 305);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(711, 144);
-            this.label6.TabIndex = 4;
-            this.label6.Text = resources.GetString("label6.Text");
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_163514131_removebg_preview;
-            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(725, 49);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(196, 196);
-            this.pictureBox4.TabIndex = 18;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_163928313_removebg_preview;
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(900, 177);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(196, 196);
-            this.pictureBox3.TabIndex = 17;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_163246609_removebg_preview;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(912, -29);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(171, 193);
-            this.pictureBox2.TabIndex = 16;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = global::InjuryCare.Properties.Resources.imagem_2025_10_03_164224623_removebg_preview;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(743, 305);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(196, 196);
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
             // 
             // Cicatrizacao
             // 
@@ -192,12 +192,12 @@
             this.Load += new System.EventHandler(this.Cicatrizacao_Load);
             this.Pan1.ResumeLayout(false);
             this.Pan1.PerformLayout();
-            this.Pan2.ResumeLayout(false);
-            this.Pan2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Pan2.ResumeLayout(false);
+            this.Pan2.PerformLayout();
             this.ResumeLayout(false);
 
         }
