66acae7b43d89497c5a2b1689c26a8e4f0dd6413
GabiDourado <gabi.dourados2xd@gmail.com>
2025-10-31 15:24:01 -0300

    Tela de referências finalizada e funcional

--- Referencias.Designer.cs
+++ Referencias.Designer.cs
@@ -28,31 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Referencias));
+            this.pnTitulo = new System.Windows.Forms.Panel();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.pnInfo = new System.Windows.Forms.Panel();
+            this.linkLabel7 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnTitulo.SuspendLayout();
+            this.pnInfo.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // pnTitulo
             // 
-            this.panel1.BackColor = System.Drawing.Color.Aquamarine;
-            this.panel1.Location = new System.Drawing.Point(187, 54);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(949, 105);
-            this.panel1.TabIndex = 0;
+            this.pnTitulo.BackColor = System.Drawing.Color.Aquamarine;
+            this.pnTitulo.Controls.Add(this.lbTitulo);
+            this.pnTitulo.Location = new System.Drawing.Point(53, 30);
+            this.pnTitulo.Name = "pnTitulo";
+            this.pnTitulo.Size = new System.Drawing.Size(1332, 71);
+            this.pnTitulo.TabIndex = 0;
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.BackColor = System.Drawing.Color.Aquamarine;
+            this.lbTitulo.Font = new System.Drawing.Font("Comic Sans MS", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lbTitulo.Margin = new System.Windows.Forms.Padding(0);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lbTitulo.Size = new System.Drawing.Size(1332, 71);
+            this.lbTitulo.TabIndex = 2;
+            this.lbTitulo.Text = "Referências";
+            this.lbTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pnInfo
+            // 
+            this.pnInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(250)))), ((int)(((byte)(220)))));
+            this.pnInfo.Controls.Add(this.linkLabel7);
+            this.pnInfo.Controls.Add(this.linkLabel6);
+            this.pnInfo.Controls.Add(this.linkLabel5);
+            this.pnInfo.Controls.Add(this.linkLabel4);
+            this.pnInfo.Controls.Add(this.linkLabel3);
+            this.pnInfo.Controls.Add(this.linkLabel2);
+            this.pnInfo.Controls.Add(this.linkLabel1);
+            this.pnInfo.Controls.Add(this.label4);
+            this.pnInfo.Controls.Add(this.label3);
+            this.pnInfo.Controls.Add(this.label2);
+            this.pnInfo.Location = new System.Drawing.Point(53, 139);
+            this.pnInfo.Name = "pnInfo";
+            this.pnInfo.Size = new System.Drawing.Size(1332, 556);
+            this.pnInfo.TabIndex = 1;
+            // 
+            // linkLabel7
+            // 
+            this.linkLabel7.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel7.AutoSize = true;
+            this.linkLabel7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel7.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel7.Location = new System.Drawing.Point(49, 476);
+            this.linkLabel7.Name = "linkLabel7";
+            this.linkLabel7.Size = new System.Drawing.Size(445, 29);
+            this.linkLabel7.TabIndex = 23;
+            this.linkLabel7.TabStop = true;
+            this.linkLabel7.Text = "https://gemini.google.com/app?hl=pt-BR";
+            this.linkLabel7.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel7.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel7_LinkClicked);
+            // 
+            // linkLabel6
+            // 
+            this.linkLabel6.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel6.AutoSize = true;
+            this.linkLabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel6.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel6.Location = new System.Drawing.Point(49, 426);
+            this.linkLabel6.Name = "linkLabel6";
+            this.linkLabel6.Size = new System.Drawing.Size(214, 29);
+            this.linkLabel6.TabIndex = 22;
+            this.linkLabel6.TabStop = true;
+            this.linkLabel6.Text = "https://chatgpt.com";
+            this.linkLabel6.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel6.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel5.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel5.Location = new System.Drawing.Point(49, 329);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(268, 29);
+            this.linkLabel5.TabIndex = 21;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "https://pin.it/1ZbXuoclU ";
+            this.linkLabel5.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel4.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel4.Location = new System.Drawing.Point(49, 281);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(794, 29);
+            this.linkLabel4.TabIndex = 20;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "https://www.flaticon.com/br/icone-gratis/tratamento-de-feridas_17306911 ";
+            this.linkLabel4.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel3.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel3.Location = new System.Drawing.Point(49, 232);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(321, 29);
+            this.linkLabel3.TabIndex = 19;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "https://hospitalamigo.com.br ";
+            this.linkLabel3.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel2.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel2.Location = new System.Drawing.Point(49, 127);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(1072, 87);
+            this.linkLabel2.TabIndex = 18;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = resources.GetString("linkLabel2.Text");
+            this.linkLabel2.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.MediumSpringGreen;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic);
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(49, 83);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(612, 29);
+            this.linkLabel1.TabIndex = 17;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.flaticon.com/br/icone-gratis/ferida_2025641 ";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(392, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Demais imagens geradas por IA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Imagens dos Botões:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Referencias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnInfo);
+            this.Controls.Add(this.pnTitulo);
             this.Name = "Referencias";
             this.Size = new System.Drawing.Size(1431, 946);
             this.Load += new System.EventHandler(this.Referencias_Load);
+            this.pnTitulo.ResumeLayout(false);
+            this.pnInfo.ResumeLayout(false);
+            this.pnInfo.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnTitulo;
+        private System.Windows.Forms.Panel pnInfo;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel7;
+        private System.Windows.Forms.LinkLabel linkLabel6;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
     }
 }
--- Referencias.cs
+++ Referencias.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,121 @@ namespace InjuryCare
 
         private void Referencias_Load(object sender, EventArgs e)
         {
+            int raio = 30;
+            GraphicsPath caminho = new GraphicsPath();
 
+            caminho.AddArc(0, 0, raio, raio, 180, 90);
+            caminho.AddArc(pnTitulo.Width - raio, 0, raio, raio, 270, 90);
+            caminho.AddArc(pnTitulo.Width - raio, pnTitulo.Height - raio, raio, raio, 0, 90);
+            caminho.AddArc(0, pnTitulo.Height - raio, raio, raio, 90, 90);
+            caminho.CloseFigure();
+
+            GraphicsPath borda = new GraphicsPath();
+
+            borda.AddArc(0, 0, raio, raio, 180, 90);
+            borda.AddArc(pnInfo.Width - raio, 0, raio, raio, 270, 90);
+            borda.AddArc(pnInfo.Width - raio, pnInfo.Height - raio, raio, raio, 0, 90);
+            borda.AddArc(0, pnInfo.Height - raio, raio, raio, 90, 90);
+            borda.CloseFigure();
+
+            pnTitulo.Region = new Region(caminho);
+            pnInfo.Region = new Region(borda);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel7_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel7.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://gemini.google.com/app?hl=pt-BR",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel1.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://www.flaticon.com/br/icone-gratis/ferida_2025641 ",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel2.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://br.freepik.com/vetores-premium/pele-lesionada-das-maos-e-procedimentos-de-limpeza-de-feridas-primeiros-socorros-para-feridas-cura-ou-tratamento-de-medicamentos-primeira-ajuda-de-emergencia-para-traumas-de-maos-humanas_30622230.htm ",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel3.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://hospitalamigo.com.br",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel4.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://www.flaticon.com/br/icone-gratis/tratamento-de-feridas_17306911 ",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel5.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://pin.it/1ZbXuoclU",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
+        }
+
+        private void linkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Indica que o link foi visitado (muda a cor)
+            linkLabel6.LinkVisited = true;
+
+            // Abre o site no navegador padrão
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://chatgpt.com",
+                UseShellExecute = true // precisa disso em .NET 5+ ou .NET 6+
+            });
         }
     }
 }
